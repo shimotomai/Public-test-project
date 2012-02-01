fa93e349a717d98889522ed888ba3921a4994d04
nanashi <nanashi@CF-J9>
2012-02-02 02:10:23 +0900

    z

---
+++ src/zzz/zzz/ZzzzActivity.java
@@ -0,0 +1,13 @@
+package zzz.zzz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ZzzzActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
